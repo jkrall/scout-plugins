6781029cf2e0cdea3d23b404dd0b551a7b96718f
Joshua Krall <josh@transfs.com>
2009-05-04 06:38:29 -0500

    Updated memory_profiler to 2.0

--- memory_profiler/memory_profiler.rb
+++ memory_profiler/memory_profiler.rb
@@ -1,21 +1,17 @@
 class MemoryProfiler < Scout::Plugin
-  def run
+  def build_report
     mem_total = @options['total_memory'].to_i
     mem_used = `ps -Ao rss`.split(/\n/)[1..-1].collect {|n| n.strip.to_i}.inject(0) {|c,i| c+i} / 1024
     mem_free = mem_total - mem_used
     mem_percent_used = (mem_used / mem_total.to_f * 100).to_i
 
-    report = {}
+    report 'Memory Total' => mem_total
+    report 'Memory Free' => mem_free
+    report 'Memory Used' => mem_used    
+    report '% Memory Used' => mem_percent_used
 
-    report['Memory Total'] = mem_total
-    report['Memory Free'] = mem_free
-    report['Memory Used'] = mem_used    
-    report['% Memory Used'] = mem_percent_used
-
-    { :report => report }
-    
   rescue Exception => e
-    body = "An error occurred profiling the memory:\n\n#{e.message}\n\n#{e.backtrace}"
-    {:error => {:subject => "Unable to Profile Memory", :body    => body}}
+    error(:subject => "Unable to Profile Memory",
+          :body => "An error occurred profiling the memory:\n\n#{e.message}\n\n#{e.backtrace}")
   end
 end
